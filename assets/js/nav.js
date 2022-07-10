3043d375132956fa4f9f6fe0c4cd99e135778c1e
Gerardo Garza <owlscloud@gmail.com>
2022-07-10 14:46:47 -0500

    repo init

---
+++ assets/js/nav.js
@@ -0,0 +1,14 @@
+const primaryNav = document.querySelector(".primary-nav");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visible = primaryNav.getAttribute("data-visible");
+
+  if (visible === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
