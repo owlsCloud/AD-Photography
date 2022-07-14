8b4163594745ae962746b339b41bd1ee8d32da60
Gerardo Garza <owlscloud@gmail.com>
2022-07-13 23:33:35 -0500

    carousel transition

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -8,6 +8,8 @@ buttons.forEach((button) => {
       .querySelector("[data-slides]");
 
     const activeSlide = slides.querySelector("[data-active]");
+
+    //gets index of next/prev slide
     let newIndex = [...slides.children].indexOf(activeSlide) + offset;
 
     if (newIndex < 0) newIndex = slides.children.length - 1;
@@ -15,6 +17,27 @@ buttons.forEach((button) => {
 
     slides.children[newIndex].dataset.active = true;
     delete activeSlide.dataset.active;
-    console.log(button);
   });
 });
+
+const changeSlide = () => {
+  //progress forward
+  const offset = 1;
+
+  //stores the element (ul) that contains the slides in a variable
+  const slides = button
+    .closest("[data-carousel]")
+    .querySelector("[data-slides]");
+
+  const activeSlide = slides.querySelector("[data-active]");
+
+  //gets index of next/prev slide
+  let newIndex = [...slides.children].indexOf(activeSlide) + offset;
+
+  if (newIndex < 0) newIndex = slides.children.length - 1;
+  if (newIndex >= slides.children.length) newIndex = 0;
+
+  slides.children[newIndex].dataset.active = true;
+  delete activeSlide.dataset.active;
+};
+setTimeout(changeSlide, 3000);
