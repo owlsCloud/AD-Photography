568ff13cc933fca433a3064f1ffa0d6f8186a9a6
Gerardo Garza <owlscloud@gmail.com>
2022-07-14 00:44:45 -0500

    working on nav

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -3,6 +3,7 @@ const buttons = document.querySelectorAll("[data-carousel-button]");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+
     const slides = button
       .closest("[data-carousel]")
       .querySelector("[data-slides]");
--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,17 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const pages = ["portfolio", "about", "winds", "social", "contact"];
+const navButtons = document.querySelectorAll("[data-nav]");
 
+navButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    changePage(button.dataset.nav);
+  });
+});
+
+const changePage = (target) => {};
+
+//responsive-----------------------------------------------
 navToggle.addEventListener("click", () => {
   const visible = primaryNav.getAttribute("data-visible");
 
