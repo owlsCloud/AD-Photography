b1427b42eaa180aa56fac096e5b3bf9cc137fd45
Gerardo Garza <owlscloud@gmail.com>
2022-07-20 01:57:26 -0500

    git func

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,25 +1,20 @@
 const primaryNav = document.querySelector(".primary-nav");
 const navToggle = document.querySelector(".mobile-nav-toggle");
-const pages = ["portfolio", "about", "winds", "social", "contact"];
 const navButtons = document.querySelectorAll("[data-nav]");
+
+const changePage = (target) => {
+  currPage = document.querySelector("[data-current-pg]");
+  newPage = document.querySelector(`#${target}`);
+
+  newPage.setAttribute("data-current-pg", true);
+  currPage.setAttribute("data-current-pg", false);
+};
 
 navButtons.forEach((button) => {
   button.addEventListener("click", () => {
     changePage(button.dataset.nav);
   });
 });
-
-const changePage = (target) => {
-  currPage = document.querySelector("[data-active-page]");
-
-  pages.forEach((page) => {
-    if (page === target) {
-      newPage = document.querySelector(`#${page}`);
-      newPage.setAttribute("data-active-page", true);
-      delete currPage.dataset.activePage;
-    }
-  });
-};
 
 //responsive-----------------------------------------------
 navToggle.addEventListener("click", () => {
